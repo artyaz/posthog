a69d70ce81e1169d3006f4738cfee37d79656d5b
Tomás Farías Santana <tomas@posthog.com>
2026-06-12 15:20:34 +0200

    feat(warehouse-sources): Support fallback keys in Temporal.io source
    A Temporal.io source can have payloads encrypted by multiple keys.
    
    Our current source configuration only supports one encryption/decryption
    key, whereas the common library codec supports 1 + any number of
    decryption keys.
    
    This adds a new `decryption_fallback_keys` configuration parameter in
    which multiple decryption keys can be added. These are then passed along
    as settings so that the codec is populated with all of them.

--- posthog/temporal/data_imports/sources/generated_configs.py
+++ posthog/temporal/data_imports/sources/generated_configs.py
@@ -1292,6 +1292,7 @@ class TemporalIOSourceConfig(config.Config):
     client_certificate: str
     client_private_key: str
     encryption_key: str | None = None
+    fallback_decryption_keys: str | None = None
 
 
 @config.config
