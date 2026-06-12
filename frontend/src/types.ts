fb71ebb882849cea81afda02f1f548017cc1edd4
Tom Owers <owerstom@gmail.com>
2026-06-12 13:28:04 +0100

    feat(data-warehouse): implement google analytics import source

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -5205,6 +5205,7 @@ export const INTEGRATION_KINDS = [
     'google-cloud-service-account',
     'google-cloud-storage',
     'google-ads',
+    'google-analytics',
     'google-search-console',
     'google-sheets',
     'linkedin-ads',
